06c131cf9beba8fa811203f9c3f6820fc744d06f
Danilo Kaizer <161129014+dnlKaizer@users.noreply.github.com>
2024-04-12 08:45:29 -0300

    Update Software

--- Trabalho/Sistema de Estoque/src/Software.java
+++ Trabalho/Sistema de Estoque/src/Software.java
@@ -1,13 +1,19 @@
-public class Software {
-    public static void main(String[] args) throws Exception {
+import java.util.Scanner;
 
+public class Software {
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        
         Estoque estoque = new Estoque();
         init(estoque);
+        listar(estoque);
     }
-
+    
+    /* Cadastra 5 produtos automaticamente */
     static void init(Estoque estoque) {
         Produto p = new Produto();
-
+        
         String nomes[] = {"Arroz", "Feijão", "Batata Frita", "Picanha Suína", "Suco de Laranja"};
         String marcas[] = {"PratoFino", "Camil", "BemBrasil", "Saudali", "Del Valle"};
         float precos[] = {11.99f, 8.89f, 41.49f, 29.99f, 4.29f};
@@ -17,11 +23,44 @@ public class Software {
             p.addMarca(marcas[i]);
             p.addPreco(precos[i]);
             p.addQuantidade(20);
-            if (estoque.inserir(p)) {
-                System.out.println(p.lerNome() + " inserido.");
-            } else {
-                System.out.println("Falha ao inserir.");
-            }
+            estoque.inserir(p);
+        }
+    }
+    
+    /* Listar todos os produtos cadatrados */
+    static void listar(Estoque estoque) {
+        
+        System.out.println();
+        System.out.println("1. Por Código");
+        System.out.println("2. Alfabética");
+        System.out.print("\nDigite a ordem: ");
+        int ordem = sc.nextInt();
+
+        Produto[] produtos = estoque.listar();
+
+        switch (ordem) {
+            /* Ordem por código (código, nome, preço e quantidade) */
+            case 1:
+                System.out.println();
+                System.out.printf("%6s \t %20s \t %8s \t %s", "CÓDIGO", "NOME", "PREÇO", "QUANTIDADE");
+                System.out.println();
+
+                for (int i = 0; i < estoque.lerNProdutos(); i++) {
+                    System.out.printf(
+                        "%6d \t %20s \t %8f \t %d",
+                        produtos[i].lerCodigo(), produtos[i].lerNome(), produtos[i].lerPreco(), produtos[i].lerQuantidade());
+                        System.out.println();
+                }
+                break;
+
+            /* Ordem alfabética (nome, código, preço) */
+            case 2:
+                
+                break;
+        
+            default:
+                System.out.println("Erro.");
+                break;
         }
     }
 
