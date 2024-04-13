8da82568a0c6c6dd5c2dc00ff1e95eb6eaef5193
dnlKaizer <danilokaizer@gmail.com>
2024-04-13 12:13:36 -0300

    Reorganização dos métodos no software
    Antigo método listar() no software, foi dividido entre 3 métodos:
        listar();
        listarTodos();
        listarPorNome();
    
    Objetivo: facilitar a implementação do método excluir() e organização

--- Trabalho/Sistema de Estoque/src/Software.java
+++ Trabalho/Sistema de Estoque/src/Software.java
@@ -101,61 +101,73 @@ public class Software {
         System.out.print("\nDigite a ordem: ");
         int ordem = sc.nextInt();
 
-        DecimalFormat moeda = new DecimalFormat("#,##0.00");
-        int nProdutos = estoque.lerNProdutos();
-        Produto[] produtos;
-        String linhaMenu = "----------------------------------------------------";
-        
         switch (ordem) {
             /* Ordem por código (código, nome, preço e quantidade) */
             case 1:
-                produtos = estoque.listarTodos();
-
-                linhaMenu += "---------------";
-                System.out.println();
-                System.out.println(linhaMenu);
-                System.out.printf("|  %-6s  |  %-20s  |  %-8s    |  %-10s  |", 
-                "CÓDIGO", "NOME", "PREÇO", "QUANTIDADE");
-                System.out.println();
-                System.out.println(linhaMenu);
-                
-                for (int i = 0; i < nProdutos; i++) {
-                    System.out.printf(
-                        "|  %-6d  |  %-20s  |  R$%8s  |  %-10d  |",
-                        produtos[i].lerCodigo(), produtos[i].lerNome(),
-                        moeda.format(produtos[i].lerPreco()),produtos[i].lerQuantidade());
-                        System.out.println();
-                }
-
-                System.out.println(linhaMenu);
+                listarTodos(estoque);
                 break;
 
             /* Ordem alfabética (nome, código, preço) */
             case 2:
-                produtos = estoque.listarPorNome();
-
-                System.out.println();
-                System.out.println(linhaMenu);
-                System.out.printf("|  %-20s  |  %-6s  |  %-8s    |", 
-                "NOME", "CÓDIGO", "PREÇO");
-                System.out.println();
-                System.out.println(linhaMenu);
-                
-                for (int i = 0; i < nProdutos; i++) {
-                    System.out.printf(
-                        "|  %-20s  |  %-6d  |  R$%8s  |",
-                        produtos[i].lerNome(), produtos[i].lerCodigo(),
-                        moeda.format(produtos[i].lerPreco()));
-                        System.out.println();
-                }
-
-                System.out.println(linhaMenu);
+                listarPorNome(estoque);
                 break;
         
             default:
-
                 System.out.println("\nERRO\n");
                 break;
         }
     }
+
+    static void listarTodos(Estoque estoque) {
+
+        DecimalFormat moeda = new DecimalFormat("#,##0.00");
+        Produto[] produtos = estoque.listarTodos();
+        int nProdutos = estoque.lerNProdutos();
+        String linhaMenu = "-------------------------------------------------------------------";
+
+        System.out.println();
+        System.out.println(linhaMenu);
+        System.out.printf("|  %-6s  |  %-20s  |  %-8s    |  %-10s  |", 
+            "CÓDIGO", "NOME", "PREÇO", "QUANTIDADE");
+        System.out.println();
+        System.out.println(linhaMenu);
+            
+        for (int i = 0; i < nProdutos; i++) {
+            System.out.printf(
+                "|  %-6d  |  %-20s  |  R$%8s  |  %-10d  |",
+                produtos[i].lerCodigo(), produtos[i].lerNome(),
+                moeda.format(produtos[i].lerPreco()),produtos[i].lerQuantidade());
+            System.out.println();
+        }
+        System.out.println(linhaMenu);
+    }
+
+    static void listarPorNome(Estoque estoque) {
+        
+        DecimalFormat moeda = new DecimalFormat("#,##0.00");
+        Produto[] produtos = estoque.listarPorNome();
+        int nProdutos = estoque.lerNProdutos();
+        String linhaMenu = "----------------------------------------------------";
+
+        System.out.println();
+        System.out.println(linhaMenu);
+        System.out.printf("|  %-20s  |  %-6s  |  %-8s    |", 
+            "NOME", "CÓDIGO", "PREÇO");
+        System.out.println();
+        System.out.println(linhaMenu);
+                
+        for (int i = 0; i < nProdutos; i++) {
+            System.out.printf(
+                "|  %-20s  |  %-6d  |  R$%8s  |",
+                produtos[i].lerNome(), produtos[i].lerCodigo(),
+                moeda.format(produtos[i].lerPreco()));
+            System.out.println();
+        }
+
+        System.out.println(linhaMenu);
+    }
+
+    // static excluir(Estoque estoque) {
+    //     listar(estoque);
+    // }
 }
