b958b8817aa24c95d108c3ef0d9339760945f411
dnlKaizer <danilokaizer@gmail.com>
2024-04-14 10:28:30 -0300

    Implementação do método detalhar()
    Reorganização da ordem das funções no código

--- Trabalho/Sistema de Estoque/src/Software.java
+++ Trabalho/Sistema de Estoque/src/Software.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Software {
 
-    static Scanner sc = new Scanner(System.in, "CP850");
+    static Scanner sc = new Scanner(System.in, "CP850"); 
     public static void main(String[] args) throws Exception {
         
         Estoque estoque = new Estoque();
@@ -18,6 +18,7 @@ public class Software {
             System.out.println("2. Excluir produto");
             System.out.println("3. Alterar produto");
             System.out.println("4. Listar produtos");
+            System.out.println("5. Detalhar produto");
             System.out.print("\nDigite o comando: ");
             comando = sc.nextInt();
 
@@ -47,6 +48,11 @@ public class Software {
                     Thread.sleep(2000);
                     break;
             
+                case 5:
+                    detalhar(estoque);
+                    Thread.sleep(1000);
+                    break;
+
                 default:
                     System.out.println("\nComando inválido. Tente novamente.");
                     Thread.sleep(1000);
@@ -94,89 +100,10 @@ public class Software {
             System.out.println("Falha ao cadastrar.");
         }
     }
-    
-    /* Listar todos os produtos cadatrados */
-    static void listar(Estoque estoque) {
-        
-        System.out.println();
-        System.out.println("1. Por Código");
-        System.out.println("2. Alfabética");
-        System.out.print("\nDigite a ordem: ");
-        int ordem = sc.nextInt();
-
-        switch (ordem) {
-            /* Ordem por código (código, nome, preço e quantidade) */
-            case 1:
-                listarTodos(estoque);
-                break;
-
-            /* Ordem alfabética (nome, código, preço) */
-            case 2:
-                listarPorNome(estoque);
-                break;
-        
-            default:
-                System.out.println("\nERRO\n");
-                break;
-        }
-    }
-
-    /* Imprime tabela dos produtos (código, nome, preço e quantidade) */
-    static void listarTodos(Estoque estoque) {
-
-        DecimalFormat moeda = new DecimalFormat("#,##0.00");
-        Produto[] produtos = estoque.listarTodos();
-        int nProdutos = estoque.lerNProdutos();
-        String linhaMenu = "-------------------------------------------------------" + 
-        "-------------------------------";
-
-        System.out.println();
-        System.out.println(linhaMenu);
-        System.out.printf("|  %-6s  |  %-20s  |  %-14s  |  %-8s    |  %-10s  |", 
-            "CÓDIGO", "NOME", "MARCA", "PREÇO", "QUANTIDADE");
-        System.out.println();
-        System.out.println(linhaMenu);
-            
-        for (int i = 0; i < nProdutos; i++) {
-            System.out.printf(
-                "|  %-6d  |  %-20s  |  %-14s  |  R$%8s  |  %-10d  |",
-                produtos[i].lerCodigo(), produtos[i].lerNome(), produtos[i].lerMarca(),
-                moeda.format(produtos[i].lerPreco()),produtos[i].lerQuantidade());
-            System.out.println();
-        }
-        System.out.println(linhaMenu);
-    }
-
-    /* Imprime tabela dos produtos (nome, código, preço) */
-    static void listarPorNome(Estoque estoque) {
-        
-        DecimalFormat moeda = new DecimalFormat("#,##0.00");
-        Produto[] produtos = estoque.listarPorNome();
-        int nProdutos = estoque.lerNProdutos();
-        String linhaMenu = "-------------------------------------------------------" + 
-        "-------------------------------";
-
-        System.out.println();
-        System.out.println(linhaMenu);
-        System.out.printf("|  %-20s  |  %-6s  |  %-14s  |  %-8s    |  %-10s  |", 
-            "NOME", "CÓDIGO", "MARCA","PREÇO", "QUANTIDADE");
-        System.out.println();
-        System.out.println(linhaMenu);
-                
-        for (int i = 0; i < nProdutos; i++) {
-            System.out.printf(
-                "|  %-20s  |  %-6d  |  %-14s  |  R$%8s  |  %-10s  |",
-                produtos[i].lerNome(), produtos[i].lerCodigo(), produtos[i].lerMarca(),
-                moeda.format(produtos[i].lerPreco()), produtos[i].lerQuantidade());
-            System.out.println();
-        }
-
-        System.out.println(linhaMenu);
-    }
 
     /* Excluir produto */
     static void excluir(Estoque estoque) {
-        listarTodos(estoque);
+        listarTodos(estoque.listarTodos());
         System.out.print("\nDigite o código do produto que deseja excluir: ");
         int codigo = sc.nextInt();
         Produto produto = estoque.buscar(codigo);
@@ -199,10 +126,10 @@ public class Software {
             }
         }
     }
-
+    
     /* Altera um atributo escolhido de um produto */
     static void alterar(Estoque estoque) {
-        listarTodos(estoque);
+        listarTodos(estoque.listarTodos());
         System.out.print("\nDigite o código do produto que deseja alterar: ");
         int codigo = sc.nextInt();
         Produto produto = estoque.buscar(codigo);
@@ -224,38 +151,153 @@ public class Software {
         System.out.print("\nDigite o comando: ");
         int caso = sc.nextInt();
         sc.nextLine();
-
+        
         System.out.println();
         switch (caso) {
             case 0:
-                System.out.println("Alteração cancelada.");
-                return;
+            System.out.println("Alteração cancelada.");
+            return;
             case 1:
-                System.out.print("Digite o novo nome: ");
-                produto.addNome(sc.nextLine());
-                break;
+            System.out.print("Digite o novo nome: ");
+            produto.addNome(sc.nextLine());
+            break;
             case 2:
-                System.out.print("Digite a nova marca: ");
-                produto.addMarca(sc.nextLine());
-                break;
+            System.out.print("Digite a nova marca: ");
+            produto.addMarca(sc.nextLine());
+            break;
             case 3:
-                System.out.print("Digite o novo preço: ");
-                produto.addPreco(sc.nextFloat());
-                break;
+            System.out.print("Digite o novo preço: ");
+            produto.addPreco(sc.nextFloat());
+            break;
             case 4:
-                System.out.print("Digite a nova quantidade: ");
-                produto.addQuantidade(sc.nextInt());
-                break;
-        
+            System.out.print("Digite a nova quantidade: ");
+            produto.addQuantidade(sc.nextInt());
+            break;
+            
             default:
-                System.out.println("Falha ao alterar.");
-                return;
+            System.out.println("Falha ao alterar.");
+            return;
         }
         System.out.println();
         if (estoque.alterar(codigo, produto)) {
             System.out.println(produto.lerNome() + " alterado com sucesso.");
         } else {
             System.out.println("Falha ao alterar.");
+        }
+    }
+
+    /* Listar todos os produtos cadatrados */
+    static void listar(Estoque estoque) {
+        
+        System.out.println();
+        System.out.println("1. Por Código");
+        System.out.println("2. Alfabética");
+        System.out.print("\nDigite a ordem: ");
+        int ordem = sc.nextInt();
+
+        switch (ordem) {
+            /* Ordem por código (código, nome, preço e quantidade) */
+            case 1:
+                listarTodos(estoque.listarTodos());
+                break;
+
+            /* Ordem alfabética (nome, código, preço) */
+            case 2:
+                listarPorNome(estoque.listarPorNome());
+                break;
+        
+            default:
+                System.out.println("\nERRO\n");
+                break;
+        }
+    }
+
+    /* Imprime tabela dos produtos (código, nome, preço e quantidade) */
+    static void listarTodos(Produto[] produtos) {
+
+        DecimalFormat moeda = new DecimalFormat("#,##0.00");
+        int nProdutos = produtos.length;
+        String linhaMenu = "-------------------------------------------------------" + 
+        "-------------------------------";
+
+        System.out.println();
+        System.out.println(linhaMenu);
+        System.out.printf("|  %-6s  |  %-20s  |  %-14s  |  %-8s    |  %-10s  |", 
+            "CÓDIGO", "NOME", "MARCA", "PREÇO", "QUANTIDADE");
+        System.out.println();
+        System.out.println(linhaMenu);
+            
+        for (int i = 0; i < nProdutos; i++) {
+            System.out.printf(
+                "|  %-6d  |  %-20s  |  %-14s  |  R$%8s  |  %-10d  |",
+                produtos[i].lerCodigo(), produtos[i].lerNome(), produtos[i].lerMarca(),
+                moeda.format(produtos[i].lerPreco()),produtos[i].lerQuantidade());
+            System.out.println();
+        }
+        System.out.println(linhaMenu);
+    }
+
+    /* Imprime tabela dos produtos (nome, código, preço) */
+    static void listarPorNome(Produto[] produtos) {
+        
+        DecimalFormat moeda = new DecimalFormat("#,##0.00");
+        int nProdutos = produtos.length;
+        String linhaMenu = "-------------------------------------------------------" + 
+        "-------------------------------";
+
+        System.out.println();
+        System.out.println(linhaMenu);
+        System.out.printf("|  %-20s  |  %-6s  |  %-14s  |  %-8s    |  %-10s  |", 
+            "NOME", "CÓDIGO", "MARCA","PREÇO", "QUANTIDADE");
+        System.out.println();
+        System.out.println(linhaMenu);
+                
+        for (int i = 0; i < nProdutos; i++) {
+            System.out.printf(
+                "|  %-20s  |  %-6d  |  %-14s  |  R$%8s  |  %-10s  |",
+                produtos[i].lerNome(), produtos[i].lerCodigo(), produtos[i].lerMarca(),
+                moeda.format(produtos[i].lerPreco()), produtos[i].lerQuantidade());
+            System.out.println();
+        }
+
+        System.out.println(linhaMenu);
+    }
+
+    static void detalhar(Estoque estoque) {
+
+        System.out.println("\nComo deseja detalhar?\n");
+        System.out.println("1. Por Código");
+        System.out.println("2. Por Nome");
+        System.out.print("\nDigite o comando: ");
+        int caso = sc.nextInt();
+        Produto produto = new Produto();
+        Produto[] vetAux = new Produto[1];
+
+        System.out.println();
+        switch (caso) {
+            case 1:
+                System.out.print("Digite o código do produto: ");
+                int codigo = sc.nextInt();
+                produto = estoque.buscar(codigo);
+                break;
+                
+            case 2:
+                sc.nextLine();
+                System.out.print("Digite o nome do produto: ");
+                String nome = sc.nextLine();
+                produto = estoque.buscarPorNome(nome); 
+                break;
+        
+            default:
+                System.out.println("Comando inválido.");
+                return;
+        }
+
+        if (produto != null) {
+            vetAux[0] = produto;
+            listarTodos(vetAux);
+        } else {
+            System.out.println("\nProduto não encontrado.");
         }
     }
 }
