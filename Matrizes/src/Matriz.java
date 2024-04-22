f443c5b02574e7cb3cefc16dee97427bfe588fcd
dnlKaizer <danilokaizer@gmail.com>
2024-04-22 19:49:15 -0300

    Implementação de métodos básicos
    Métodos:
        lerIndice(i, j)
        lerLinha(i)
        lerColuna(j)
    
    Adicionado comentários nos métodos

--- Matrizes/src/Matriz.java
+++ Matrizes/src/Matriz.java
@@ -2,30 +2,70 @@ public class Matriz {
     
     int nColunas;
     int nLinhas;
-    int[][] indice;
+    double[][] indice;
 
-    boolean inserirMatriz(int matriz[][]) {
-        if (matriz.length > 0) {
-            nLinhas = matriz.length;
-            nColunas = matriz[0].length;
-        } else {
-            return false;
-        }
+    /**
+     * Insere uma matriz ao objeto <code>Matriz</code>. Salva o tamanho das linhas em <em>nLinhas</em> e das colunas em <em>nColunas</em>. 
+     * @param matriz de índices */
+    void inserirMatriz(double matriz[][]) {
+        nLinhas = matriz.length;
+        nColunas = matriz[0].length;
+        indice = new double[nLinhas][nColunas];
+
         for (int i = 0; i < nLinhas; i++) {
             for (int j = 0; j < nColunas; j++) {
                 indice[i][j] = matriz[i][j];
             }
         }
-        return true;
     }
 
-    int[][] lerMatriz() {
-        int matriz[][] = new int[nLinhas][nColunas];
+    /**
+     * Retorna uma <em>cópia</em> da matriz do Objeto
+     * @return matriz de índices
+      */
+    double[][] lerMatriz() {
+        double matriz[][] = new double[nLinhas][nColunas];
         for (int i = 0; i < nLinhas; i++) {
             for (int j = 0; j < nColunas; j++) {
                 matriz[i][j] = indice[i][j];
             }
         }
         return matriz;
+    }
+
+    /**
+     * Retorna o valor guardado pelos índices i e j.
+     * @param i índice da linha
+     * @param j índice da coluna 
+     * @return valor do índice
+      */
+    double lerIndice(int i, int j) {
+        return indice[i][j];
+    }
+
+    /**
+     * Retorna uma linha inteira da matriz
+     * @param i índice da linha 
+     * @return vetor linha
+      */
+    double[] lerLinha(int i) {
+        double[] vetAux = new double[nColunas];
+        for (int j = 0; j < nColunas; j++) {
+            vetAux[j] = indice[i][j];
+        }
+        return vetAux;
+    }
+
+    /**
+     * Retorna uma coluna inteira da matriz
+     * @param j índice da coluna
+     * @return vetor coluna
+      */
+    double[] lerColuna(int j) {
+        double[] vetAux = new double[nLinhas];
+        for (int i = 0; i < nLinhas; i++) {
+            vetAux[i] = indice[i][j];
+        }
+        return vetAux;
     }
 }
