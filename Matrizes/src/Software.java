b83081b3f798989f0ee80f0d966684ca85c88208
dnlKaizer <danilokaizer@gmail.com>
2024-04-23 13:58:27 -0300

    Implementados os métodos que imprimem as operações elementares

--- Matrizes/src/Software.java
+++ Matrizes/src/Software.java
@@ -1,23 +1,17 @@
+import java.text.DecimalFormat;
+
 public class Software {
     public static void main(String[] args) throws Exception {
         
-        Matriz matriz1 = new Matriz();
-        double[][] matAux = {{2, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-        matriz1.inserirMatriz(matAux);
+        Matriz matriz = new Matriz();
+        double[][] matAux = {{1, 0, 0}, {0, 0.5, 0}, {0, 0, 1}};
+        matriz.inserirMatriz(matAux);
 
-        Matriz matriz2 = new Matriz();
-        matriz2.inserirTamanho(3, 3);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                matriz2.inserirValor((i + j + 1), i, j);
-            }
-        }
-        
-        Matriz resultado = new Matriz();
-        resultado.inserirMatriz(multiplicaMatriz(matriz1, matriz2).lerMatriz());
-
-        imprimirMatriz(resultado.lerMatriz());
+        imprimirMatriz(matriz.lerMatriz());
+        somarMultiplo(matriz, 0, 1, 2);
     }
+
+    static DecimalFormat df = new DecimalFormat("#,##0.##");
 
     static Matriz somaMatriz(Matriz m1, Matriz m2) {
         int nLinhas = m1.lerNLinhas();
@@ -57,6 +51,30 @@ public class Software {
         return null;
     }
 
+    static void trocarLinhas(Matriz matriz, int index1, int index2) {
+        System.out.println("\nL" + (index1 + 1) + " <-> L" + (index2 + 1) + "\n");
+        matriz.trocarLinhas(index1, index2);
+        imprimirMatriz(matriz.lerMatriz());
+    }
+
+    static void multiplicarEscalar(Matriz matriz, int index, int k) {
+        System.out.println("\nL" + (index + 1) + " -> " + df.format(k) + " x L" + (index + 1) + "\n");
+        matriz.multiplicarEscalar(index, k);
+        imprimirMatriz(matriz.lerMatriz());
+    }
+
+    static void somarLinha(Matriz matriz, int index1, int index2) {
+        System.out.println("\nL" + (index1 + 1) + " + L" + (index2 + 1) + " -> L" + (index1 + 1) + "\n");
+        matriz.somarLinha(index1, index2);
+        imprimirMatriz(matriz.lerMatriz());
+    }
+
+    static void somarMultiplo(Matriz matriz, int index1, int index2, double k) {
+        System.out.println("\nL" + (index1 + 1) + " + " + df.format(k) + " x L" + (index2 + 1) + " -> L" + (index1 + 1) + "\n");
+        matriz.somarMultiplo(index1, index2, k);
+        imprimirMatriz(matriz.lerMatriz());
+    }
+
     static double multiplicaVetor(double[] vet1, double[] vet2) {
         int tamanho = vet1.length;
         double result = 0;
@@ -80,7 +98,7 @@ public class Software {
     static void imprimirMatriz(double matriz[][]) {
         for(int i = 0; i < matriz.length; i++) {
             for(int j = 0; j < matriz[0].length; j++) {
-                System.out.print(matriz[i][j]);
+                System.out.print(df.format(matriz[i][j]));
                 if(j != matriz[0].length - 1) {
                     System.out.print("\t");
                 }
