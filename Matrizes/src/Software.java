921a198ba48fd7a5731fbbc3861a543b97f279fc
dnlKaizer <danilokaizer@gmail.com>
2024-04-24 15:25:33 -0300

    Comentando métodos no software

--- Matrizes/src/Software.java
+++ Matrizes/src/Software.java
@@ -6,7 +6,6 @@ public class Software {
         Matriz matriz = new Matriz();
         double[][] matAux = {{0, 2, 2}, {0, 5, 4}, {0, 8, 5}, {0, 3, 11}};
         matriz.inserirMatriz(matAux);
-
         imprimirMatriz(matriz);
         escalonar(matriz);
 
@@ -14,6 +13,12 @@ public class Software {
 
     static DecimalFormat df = new DecimalFormat("#,##0.##");
 
+    /**
+     * Soma duas matrizes do tipo <code>Matriz</code>.
+     * @param m1 matriz 1
+     * @param m2 matriz 2
+     * @return <code>null</code> se não podem ser somadas, ou a <code>Matriz</code> resultante
+      */
     static Matriz somaMatriz(Matriz m1, Matriz m2) {
         int nLinhas = m1.lerNLinhas();
         int nColunas = m1.lerNColunas();
@@ -33,6 +38,12 @@ public class Software {
         return null;
     }
 
+    /**
+     * Multiplica duas matrizes do tipo <code>Matriz</code>
+     * @param m1 matriz 1
+     * @param m2 matriz 2
+     * @return <code>null</code> se não podem ser multplicadas, ou a <code>Matriz</code> resultante
+      */
     static Matriz multiplicaMatriz(Matriz m1, Matriz m2) {
         if(m1.lerNColunas() == m2.lerNLinhas()) {
             int nLinhas = m1.lerNLinhas();
@@ -51,6 +62,12 @@ public class Software {
         }
         return null;
     }
+    /**
+     * Multiplica dois vetores do tipo <code>double</code>. <strong>Não funciona</strong> para vetores de tamanhos diferentes.
+     * @param vet1 vetor 1
+     * @param vet2 vetor 2
+     * @return resultado <code>double</code>
+      */
     static double multiplicaVetor(double[] vet1, double[] vet2) {
         int tamanho = vet1.length;
         double result = 0;
@@ -60,6 +77,10 @@ public class Software {
         return result;
     }
 
+    /**
+     * Imprime os processos para eliminação gaussiana da matriz. <strong>Não altera</strong> a matriz original.
+     * @param matrizOriginal matriz a ser escalonada
+      */
     static void escalonar(Matriz matrizOriginal) {
         Matriz matriz = new Matriz();
         matriz.inserirMatriz(matrizOriginal.lerMatriz());
@@ -114,24 +135,50 @@ public class Software {
         }
     }   
     
+    /**
+     * Imprime o processo de troca de linha da matriz
+     * @param matriz
+     * @param index1 index da primeira linha
+     * @param index2 index da segunda linha
+      */
     static void trocarLinhas(Matriz matriz, int index1, int index2) {
         System.out.println("\nL" + (index1 + 1) + " <-> L" + (index2 + 1));
         matriz.trocarLinhas(index1, index2);
         imprimirMatriz(matriz);
     }
 
+    /**
+     * Imprime o processo de multiplicar linha por escalar de uma matriz
+     * @param matriz
+     * @param index index da linha
+     * @param k escalar
+      */
     static void multiplicarLinhaPorEscalar(Matriz matriz, int index, double k) {
         System.out.println("\nL" + (index + 1) + " -> " + df.format(k) + " x L" + (index + 1));
         matriz.multiplicarEscalar(index, k);
         imprimirMatriz(matriz);
     }
 
+    /**
+     * Imprime o processo de soma de duas linhas da matriz
+     * @param matriz
+     * @param index1 index da linha que será alterada
+     * @param index2 index da linha que será somada
+      */
     static void somarLinhas(Matriz matriz, int index1, int index2) {
         System.out.println("\nL" + (index1 + 1) + " + L" + (index2 + 1) + " -> L" + (index1 + 1));
         matriz.somarLinha(index1, index2);
         imprimirMatriz(matriz);
     }
 
+
+    /**
+     * Imprime o processo de somar uma linha com múltiplo de outra
+     * @param matriz
+     * @param index1 index da linha que será alterada 
+     * @param index2 index da linha que será somada
+     * @param k escalar
+      */
     static void somarMultiploLinha(Matriz matriz, int index1, int index2, double k) {
         System.out.println("\nL" + (index1 + 1) + " + " + df.format(k) + " x L" + (index2 + 1) + " -> L" + (index1 + 1));
         matriz.somarMultiplo(index1, index2, k);
