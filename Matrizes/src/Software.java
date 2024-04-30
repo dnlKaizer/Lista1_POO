22746eb4c5e25147d38ad38b48888c098d2cddb9
dnlKaizer <danilokaizer@gmail.com>
2024-04-30 18:05:52 -0300

    Remoção dos métodos geradores

--- Matrizes/src/Software.java
+++ Matrizes/src/Software.java
@@ -5,15 +5,21 @@ public class Software {
     public static void main(String[] args) throws Exception {
         
         Matriz matriz = new Matriz();
-        double[][] matAux = {{2, 6, 2, 2}, {-3, -8, 0, 2}, {4, 9, 2, 3}};
+        double[][] matAux = {{0, 1, 2}, {0, 0, 1}, {1, 0, 0}};
         matriz.inserirMatrizDeIndices(matAux);
-        op.adicionarMatriz(matriz);
 
-        Matriz[] resultante = op.decomposicaoLU(matriz);
-        imprimirMatriz(resultante[0]);
-        imprimirMatriz(resultante[1]);
-        resultante = op.metodoGaussJordan(resultante[0]);
-        imprimirProcessos(resultante);
+        Matriz[] resultante = op.decomposicaoLU(matriz, null);
+        if (resultante != null) {
+            imprimirMatriz(resultante[0]);
+            imprimirMatriz(resultante[1]);
+            imprimirMatriz(resultante[2]);
+    
+            Matriz result = op.multiplicaMatriz(resultante[2], resultante[1]);
+            result = op.multiplicaMatriz(result, resultante[0]);
+            imprimirMatriz(result);
+        } else {
+            System.out.println("Nula");
+        }
     }
 
     static Operar op = new Operar();
@@ -21,83 +27,6 @@ public class Software {
     static DecimalFormat df = new DecimalFormat("#,##0.##");
 
     static Random random = new Random();
-
-    /**
-     * Gera uma matriz invertível, de tamanho entre 2 e 5.
-     * @return matriz invertível
-      */
-    static Matriz gerarMatrizInvertivel() {
-        int tamanho = 2 + random.nextInt(4);
-        Matriz matriz = gerarIdentidade(tamanho);
-        int nLinhas = matriz.lerNLinhas();
-        int caso;
-
-        for (int i = 0; i < (random.nextInt(12) + 4) * tamanho; i++) {
-            caso = random.nextInt(3);
-            int linha1 = random.nextInt(nLinhas);
-            int linha2;
-            do {
-                linha2 = random.nextInt(nLinhas);
-            } while (linha1 == linha2);
-
-            int escalar;
-            do {
-                escalar = -3 + random.nextInt(7);
-            } while (escalar == 0);
-
-            switch (caso) {
-                case 0:
-                    matriz.multiplicarEscalar(linha1, escalar);
-                break;
-    
-                case 1:
-                    matriz.trocarLinhas(linha1, linha2);
-                break;
-    
-                case 2:
-                    matriz.somarMultiplo(linha1, linha2, escalar);
-                break;
-            }
-        }
-
-        return matriz;
-    }
-
-    /**
-     * Gera uma matriz identidade
-     * @param tamanho da matriz
-     * @return matriz identidade
-      */
-    static Matriz gerarIdentidade(int tamanho) {
-        Matriz matriz = new Matriz();
-        matriz.inserirTamanho(tamanho, tamanho);
-
-        for (int i = 0; i < tamanho; i++) {
-            for (int j = 0; j < tamanho; j++) {
-                if(i == j) {
-                    matriz.inserirValor(1, i, j);
-                } else {
-                    matriz.inserirValor(0, i, j);
-                }
-            }
-        }
-
-        return matriz;
-    }
-
-    /**
-     * Gera vetor com entradas no intervalo [-10, 10]
-     * @param tamanho do vetor
-     * @return vetor gerado
-      */
-    static double[] geraVetor(int tamanho) {
-        double[] vetor = new double[tamanho];
-        for (int i = 0; i < tamanho; i++) {
-            vetor[i] = random.nextInt(21) - 10;
-        }
-        return vetor;
-    }
-
 
     static void imprimirMatriz(Matriz matriz) {
         System.out.println();
