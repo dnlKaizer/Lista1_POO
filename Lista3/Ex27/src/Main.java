17b20a8959518ede46393ad578210f68acbe54df
dnlKaizer <danilokaizer@gmail.com>
2024-08-03 09:56:44 -0300

    Método imprimirVetor()

--- Lista3/Ex27/src/Main.java
+++ Lista3/Ex27/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Protozoario protozoario = new Protozoario();
+        imprimirVetor(protozoario.getGenotipo());
+        protozoario.mutate();
+        imprimirVetor(protozoario.getGenotipo());
+    }
+
+    static void imprimirVetor(int[] vet) {
+        int tam = vet.length;
+        System.out.println();
+        System.out.print("[");
+        for (int i = 0; i < tam; i++) {
+            System.out.print(vet[i]);
+            if (i != tam - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+        System.out.println();
     }
 }
