5f05572605d3feba11839b43f009ff07789220ff
dnlKaizer <danilokaizer@gmail.com>
2024-08-03 11:05:18 -0300

    Métodos getGenotipo(), getClone(), mate() e reformulando constructors

--- Lista3/Ex27/src/Protozoario.java
+++ Lista3/Ex27/src/Protozoario.java
@@ -1,17 +1,25 @@
 import java.util.Random;
 
 public class Protozoario {
-    int[] genotipo = new int[10];
+    private int[] genotipo;
+    private String string = "";
+    private static Random random = new Random();
     
-    public Protozoario() {
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            genotipo[i] = random.nextInt(0, 4);
-        }
-    }
-
     private Protozoario(int[] genotipo) {
         this.genotipo = genotipo;
+        if (random.nextInt(100) < 7) {
+            this.mutate();
+        }
+        for (int i : genotipo) {
+            this.string += i;
+        }
+    }
+    public static Protozoario getInstance() {
+        int[] genotipoAux = new int[10];
+        for (int i = 0; i < 10; i++) {
+            genotipoAux[i] = random.nextInt(0, 4);
+        }
+        return new Protozoario(genotipoAux);
     }
     public static Protozoario getInstance(int[] genotipo) {
         if (genotipo == null || genotipo.length != 10) {
@@ -28,13 +36,31 @@ public class Protozoario {
     public int[] getGenotipo() {
         return genotipo.clone();
     }
+    public String toString() {
+        return string;
+    }
+
+    public Protozoario getClone() {
+        Protozoario clone = Protozoario.getInstance(genotipo);
+        return clone;
+    }
 
     public void mutate() {
-        Random random = new Random();
         int index = random.nextInt(0, 10);
         genotipo[index]--;
         if (genotipo[index] < 0) {
             genotipo[index] += 2;
         }
     }
+
+    public Protozoario mate(Protozoario parceiro) {
+        int[] genotipoP = parceiro.getGenotipo();
+        for (int i = 0; i < 10; i++) {
+            if (random.nextBoolean()) {
+                genotipoP[i] = genotipo[i];
+            }
+        }
+        return new Protozoario(genotipoP);
+    }
+
 }
