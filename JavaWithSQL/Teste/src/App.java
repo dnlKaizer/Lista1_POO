f893d9353a3fb40f7a23f6b239d60fb54a054289
dnlKaizer <danilokaizer@gmail.com>
2024-08-03 15:52:25 -0300

    Teste de leitura de dados do banco

--- JavaWithSQL/Teste/src/App.java
+++ JavaWithSQL/Teste/src/App.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import DAO.UsuarioDAO;
 import entity.Usuario;
 
@@ -6,5 +8,11 @@ public class App {
 
         Usuario user = Usuario.getInstance(1, "Roger Guedes", "rogerin", "rogerin1234");
         new UsuarioDAO().cadastrarUsuario(user);
+        List<Usuario> users = new UsuarioDAO().lerUsuario();
+        user = users.get(0);
+        System.out.println(user.getCodigo());
+        System.out.println(user.getNome());
+        System.out.println(user.getLogin());
+        System.out.println(user.getSenha());
     }
 }
