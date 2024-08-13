a0e52187625fe4d6d72f326215ee2b8600499571
dnlKaizer <danilokaizer@gmail.com>
2024-08-13 15:57:16 -0300

    Método copy() + código auto gerado

--- Trabalho/Parte 2/Sistema de Estoque/src/Venda.java
+++ Trabalho/Parte 2/Sistema de Estoque/src/Venda.java
@@ -3,16 +3,25 @@ public class Venda {
     Data data;
     Carrinho carrinho;
 
-    private Venda(int codigo, Data data, Carrinho carrinho) {
-        this.codigo = codigo;
+    static int codigoAutoGerado;
+
+    private Venda(Carrinho carrinho, Data data) {
+        codigoAutoGerado++;
+        this.codigo = codigoAutoGerado;
         this.data = data;
         this.carrinho = carrinho;
     }
-    public static Venda getInstance(int codigo, Data data, Carrinho carrinho) {
-        if (codigo <= 0) return null;
+    public static Venda getInstance(Carrinho carrinho, Data data) {
         if (data == null) return null;
         if (carrinho == null) return null;
-        return new Venda(codigo, data, carrinho);
+        return new Venda(carrinho, data);
+    }
+
+    // Constructor para o copy, não precisa de método fábrica
+    private Venda(int codigo, Carrinho carrinho, Data data) {
+        this.codigo = codigo;
+        this.data = data;
+        this.carrinho = carrinho;
     }
 
     public int getCodigo() {
@@ -23,5 +32,9 @@ public class Venda {
     }
     public Carrinho getCarrinho() {
         return carrinho.copy();
+    }
+
+    public Venda copy() {
+        return new Venda(codigo, carrinho, data);
     }
 }
