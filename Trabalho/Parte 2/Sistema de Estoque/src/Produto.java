5f028a38d8f6e6b1f49c96932072c47a32888aaa
dnlKaizer <danilokaizer@gmail.com>
2024-08-13 15:51:35 -0300

    Adição do código auto gerado

--- Trabalho/Parte 2/Sistema de Estoque/src/Produto.java
+++ Trabalho/Parte 2/Sistema de Estoque/src/Produto.java
@@ -5,6 +5,24 @@ public class Produto {
     private float preco;
     private int quantidade;
 
+    private static int codigoAutoGerado;
+
+    private Produto(String nome, String marca, float preco, int quantidade) {
+        codigoAutoGerado++;
+        this.codigo = codigoAutoGerado;
+        this.nome = nome;
+        this.marca = marca;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+    public static Produto getInstance(String nome, String marca, float preco, int quantidade) {
+        if (nome.length() <= 0) return null;
+        if (marca.length() <= 0) return null;
+        if (preco < 0) return null;
+        if (quantidade < 0) return null;
+        return new Produto(nome, marca, preco, quantidade);
+    }
+
     private Produto(int codigo, String nome, String marca, float preco, int quantidade) {
         this.codigo = codigo;
         this.nome = nome;
@@ -12,7 +30,6 @@ public class Produto {
         this.preco = preco;
         this.quantidade = quantidade;
     }
-
     public static Produto getInstance(int codigo, String nome, String marca, float preco, int quantidade) {
         if (codigo < 0) return null;
         if (nome.length() <= 0) return null;
@@ -22,18 +39,6 @@ public class Produto {
         return new Produto(codigo, nome, marca, preco, quantidade);
     }
 
-    // codigo = 0
-    public static Produto getInstance(String nome, String marca, float preco, int quantidade) {
-        if (nome.length() <= 0) return null;
-        if (marca.length() <= 0) return null;
-        if (preco < 0) return null;
-        if (quantidade < 0) return null;
-        return new Produto(0, nome, marca, preco, quantidade);
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
     public int getCodigo() {
         return codigo;
     }
