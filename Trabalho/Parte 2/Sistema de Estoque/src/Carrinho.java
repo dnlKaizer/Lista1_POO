711b984d845d509d3d7c4f2fe31c07be32c7ab76
dnlKaizer <danilokaizer@gmail.com>
2024-08-15 11:22:06 -0300

    Removendo copy() + ajustes

--- Trabalho/Parte 2/Sistema de Estoque/src/Carrinho.java
+++ Trabalho/Parte 2/Sistema de Estoque/src/Carrinho.java
@@ -5,23 +5,9 @@ public class Carrinho {
     public Carrinho() {
         itens = new Item[10];
     }
-    private Carrinho(Carrinho car) {
-        this.itens = car.copyItens();
-        this.nItens = car.getNItens();
-    }
-    private Item[] copyItens() {
-        Item[] itensAux = new Item[this.itens.length];
-        for (int i = 0; i < this.nItens; i++) {
-            itensAux[i] = this.itens[i].copy();
-        }
-        return itensAux;
-    }
 
     public int getNItens() {
         return nItens;
-    }
-    public Item[] getItens() {
-        return copyItens();
     }
 
     public boolean addItem(Item item) {
@@ -38,7 +24,7 @@ public class Carrinho {
         return true;
     }
 
-    public Item buscar(int pos) {
+    public Item buscarItem(int pos) {
         if (pos >= 0 && pos < this.nItens) return itens[pos].copy();
         else return null;
     }
@@ -46,11 +32,18 @@ public class Carrinho {
     // Retorna a posição do item caso ele já exista, caso contrário retorna -1
     private int verificarItemCarrinho(Item item) {
         for (int i = 0; i < this.nItens; i++) {
-            if (this.itens[i].getProduto().getCodigo() == item.getProduto().getCodigo()) {
+            if (this.itens[i].getCdProduto() == item.getCdProduto()) {
                 return i;
             }
         }
         return -1;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+        "{ Itens: [%s] }", 
+        this.arrayItensToString());
     }
 
     private void ampliarCarrinho() {
@@ -60,17 +53,7 @@ public class Carrinho {
         }
         this.itens = itensAux;
     }
-
-    public Carrinho copy() {
-        return new Carrinho(this);
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-        "{ Itens: [%s] }", 
-        this.arrayItensToString());
-    }
+    
     private String arrayItensToString() {
         String str = "";
         for (int i = 0; i < nItens; i++) {
