74cae69857ae4637207dc69c6353e8ac1db478d5
dnlKaizer <danilokaizer@gmail.com>
2024-08-17 09:55:25 -0300

    Adicionando nmCliente

--- Trabalho/Parte 2/Sistema de Estoque/src/Venda.java
+++ Trabalho/Parte 2/Sistema de Estoque/src/Venda.java
@@ -2,24 +2,27 @@ public class Venda {
     private int cdVenda;
     private Data data;
     private Carrinho carrinho;
+    private String nmCliente;
 
     static int codigoAutoGerado;
 
-    private Venda(int cdVenda, Carrinho carrinho, Data data) {
+    private Venda(int cdVenda, Carrinho carrinho, Data data, String nmCliente) {
         this.cdVenda = cdVenda;
         this.data = data;
         this.carrinho = carrinho;
+        this.nmCliente = nmCliente;
     }
-    private Venda(Carrinho carrinho, Data data) {
+    private Venda(Carrinho carrinho, Data data, String nmCliente) {
         codigoAutoGerado++;
         this.cdVenda = codigoAutoGerado;
         this.data = data;
         this.carrinho = carrinho;
+        this.nmCliente = nmCliente;
     }
-    public static Venda getInstance(Carrinho carrinho, Data data) {
+    public static Venda getInstance(Carrinho carrinho, Data data, String nmCliente) {
         if (data == null) return null;
         if (carrinho == null) return null;
-        return new Venda(carrinho, data);
+        return new Venda(carrinho, data, nmCliente);
     }
 
     public int getCdVenda() {
@@ -44,7 +47,7 @@ public class Venda {
     }
 
     public Venda copy() {
-        return new Venda(this.cdVenda, this.carrinho.copy(), this.data.copy());
+        return new Venda(this.cdVenda, this.carrinho.copy(), this.data.copy(), this.nmCliente);
     }
 
     @Override
