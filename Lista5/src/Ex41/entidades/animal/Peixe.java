3db818d858b1c34c27539c6183bdd81ed828b664
dnlKaizer <danilokaizer@gmail.com>
2024-09-19 12:28:58 -0300

    Classe Peixe

---
+++ Lista5/src/Ex41/entidades/animal/Peixe.java
@@ -0,0 +1,19 @@
+package Ex41.entidades.animal;
+
+public class Peixe extends Animal {
+
+    public Peixe() {
+        super("", 0);
+    }
+
+    @Override
+    public void caminhar() {
+        super.caminhar();
+    }
+
+    @Override
+    public void comer() {
+        
+    }
+    
+}
